b46ba9b32fa269a06de4405332eb867504c8895e
Tatiana Molchanova <tatuka.08@mail.ru>
2023-09-06 13:43:17 -0400

    third

--- main.py
+++ main.py
@@ -16,15 +16,25 @@ def get_cook_book():
     return cook_book
 
 def get_shop_list_by_dishes(person_count, dishes, cook_book):
-    shop_list = dict()
+    shop_list = []
     for dish in dishes:
         if dish in cook_book:
             for ingredients in cook_book[dish]:
                 product = ingredients['product']
                 quantity = int(ingredients['quantity']) * int(person_count)
                 measure = ingredients['measure']
-                shop_list[product, measure] = quantity
-    print(shop_list)
+                shop_list.append([product, measure, quantity])
+    shop_dict = {}
+    measure_dict = {}
+    for product, measure, quantity in shop_list:
+        measure_dict[product] = measure
+        shop_dict[product] = shop_dict.get(product, 0) + quantity
+
+    shop_dict_final = {}
+    for item in shop_dict.items():
+        shop_dict_final[item[0]] = {'measure': measure_dict.get(item[0]), 'quantity': item[1]}
+
+    print(shop_dict_final)
 
 cook_book = get_cook_book()
 get_shop_list_by_dishes(2, ['Омлет', 'Омлет'], cook_book)
